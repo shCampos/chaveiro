5b8abe2a4d73706075c5e594a19e0726465388ac
shcampos <samuel-hc@hotmail.com>
2020-08-03 22:15:14 -0400

    feat: check if specie is in db

--- robots/search-robots/core.js
+++ robots/search-robots/core.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 /* 
 https://core.ac.uk/api-v2/articles/search/title%3Ataxonomy?page=1&pageSize=10&metadata=true&fulltext=false&citations=false&similar=false&duplicate=false&urls=false&faithfulMetadata=false&apiKey=n2hECBkWOUXH351r8TxMfGPYFgbI9uoa
 */
-
 async function robot(content) {
   let queryParams = {
     page: 1,
--- robots/search-robots/index.js
+++ robots/search-robots/index.js
@@ -1,10 +1,14 @@
+let { checkSpecieInDb } = require('../../firebase.js')
 const searchRobots = {
   mycobank: require('./mycobank.js'),
   core: require('./core.js')
 }
 
-function robot(content) {  
-  content.searchParams.kingdon=='Fungi'?searchRobots.mycobank(content):searchRobots.core(content)
+function robot(content) {
+  content.species.forEach(el => {
+    checkSpecieInDb(el.scientificName)&&
+    content.searchParams.kingdon=='Fungi'?searchRobots.mycobank(content):searchRobots.core(content)
+  })
 }
 
 module.exports = robot
